fee3dcb0af697b2750c9758fc0d71ba4f309f1b3
sheraz888444 <itssheraz78618@gmail.com>
2025-07-21 16:12:45 +0500

    added  new components Navbar,news,newsItems

---
+++ src/components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Newsitems from './Newsitems'
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+    this is a news component
+    <Newsitems/>
+    
+
+      </div>
+    )
+  }
+}
---
+++ src/components/Newsitems.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Newsitems extends Component {
+  render() {
+    return (
+      <div>
+        this is news item component 2 
+        
+      </div>
+    )
+  }
+}
