8c411589c08041a8e02ad79119c2ffcd5e21dbca
sheraz888444 <itssheraz78618@gmail.com>
2025-07-22 13:59:20 +0500

    added Api from newsapi  to improve news components

--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export default class Newsitems extends Component {
   render() {
+  let   {title,description,imageUrl,}=this.props;
     return (
       <div>
-        this is news item component 2 
+        <div className="card" style={{width: "18rem"}}>
+  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <a href="/newsdetail/" className=" btn btn-primary">Read More</a>
+  </div>
+</div>
         
       </div>
     )
